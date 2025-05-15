7313308fbb3a4dcf62d2445ef154c4d8e6b6d95c
Walter Alejandro Valdiviezo <alejoooo4848@gmail.com>
2025-05-14 22:12:14 -0300

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mi_paquete_ejemplo",
+    version="0.1",
+    packages=find_packages(),
+    description="Un ejemplo simple de paquete",
+    author="Tu Nombre",
+    author_email="tunombre@example.com",
+)
